7c6f6dcbcee3ae78e0f8934f319dcf4cd233822f
kadr0id <kadruk@gmail.com>
2018-01-19 02:03:05 +0200

    inheritance

---
+++ app/src/main/java/oop/inheritance/TV.java
@@ -0,0 +1,34 @@
+package oop.inheritance;
+
+//same TV brand, but different model
+public class TV {
+   private String model;
+   private String brand;
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public TV( String brand){
+
+        this.brand = brand;
+    }
+
+
+    public void on() {
+    }
+
+    public void off() {
+    }
+
+    public void showsTVChannels() {
+    }
+
+    public void function() {
+        on();
+        off();
+        showsTVChannels();
+    }
+}
+
